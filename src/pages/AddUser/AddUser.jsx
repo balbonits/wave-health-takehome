01b8fe1c75508aad98b1b5cecf9186aa4c1fcb2d
balbonits <rjdofficemail@gmail.com>
2025-07-31 22:16:38 -0700

    fixed utils/storage path in AddUser

--- src/pages/AddUser/AddUser.jsx
+++ src/pages/AddUser/AddUser.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { addLocalUser } from '../utils/storage';
+import { addLocalUser } from '../../utils/storage';
 import './AddUser.css';
 
 const AddUser = () => {
